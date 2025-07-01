f1149ba50bec2ef53743899e641f20cc2c990232
binipk <mingyoku93@gmail.com>
2025-07-01 12:17:13 +0900

    Controller has been updated

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!😁✅";
+        return "I'm alive!!!!!!!😁✅";
     }
 
     @GetMapping("/plus")
